bdc8db155d58118c4063447b5261d11549f1827a
boite <boite@users.noreply.github.com>
2016-07-12 23:17:14 +0100

    Trivial simplification of logic
    and fix white space.

--- src/FstabLine.php
+++ src/FstabLine.php
@@ -11,23 +11,23 @@ class FstabLine
     {
         return $this->content;
     }
-    
+
     public function setContent($content)
     {
         $this->content = $content;
-        
+
         if (trim($content, " \t")=='') {
             $this->type = 'empty';
+            return $this;
         }
         if (substr($content, 0, 1)=='#') {
             $this->type = 'comment';
-        }
-        if ($this->type) {
             return $this;
         }
+
         $this->type = 'mount';
-        
-        
+
+
         // Remove trailing tabs
         $line = trim($this->content, "\t");
         $line = str_replace(' ', "\t", $line);
@@ -35,7 +35,7 @@ class FstabLine
         $line = str_replace("\t\t\t", "\t", $line);
         $line = str_replace("\t\t\t", "\t", $line);
         $line = str_replace("\t\t", "\t", $line);
-        
+
         // Split into parts
         $part = explode("\t", $line);
         if (count($part)!=6) {
@@ -50,51 +50,51 @@ class FstabLine
 
         return $this;
     }
-    
+
     protected $type;
     public function getType()
     {
         return $this->type;
     }
-    
+
     public function setType($type)
     {
         $this->type = $type;
         return $this;
     }
-    
-    
+
+
     protected $fileSystem;
     public function getFileSystem()
     {
         return $this->fileSystem;
     }
-    
+
     public function setFileSystem($fileSystem)
     {
         $this->fileSystem = $fileSystem;
         return $this;
     }
-    
+
     protected $mountPoint;
     public function getMountPoint()
     {
         return $this->mountPoint;
     }
-    
+
     public function setMountPoint($mountPoint)
     {
         $this->mountPoint = $mountPoint;
         return $this;
     }
-    
+
     protected $fileSystemType;
-    
+
     public function getFileSystemType()
     {
         return $this->fileSystemType;
     }
-    
+
     public function setFileSystemType($fileSystemType)
     {
         $this->fileSystemType = $fileSystemType;
@@ -105,37 +105,37 @@ class FstabLine
     {
         return $this->options;
     }
-    
+
     public function setOptions($options)
     {
         $this->options = $options;
         return $this;
     }
-    
+
     protected $dump;
     public function getDump()
     {
         return $this->dump;
     }
-    
+
     public function setDump($dump)
     {
         $this->dump = $dump;
         return $this;
     }
-    
+
     protected $pass;
     public function getPass()
     {
         return $this->pass;
     }
-    
+
     public function setPass($pass)
     {
         $this->pass = $pass;
         return $this;
     }
-    
+
     public function render()
     {
         switch ($this->getType()) {
